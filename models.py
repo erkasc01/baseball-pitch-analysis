cd941b0ec91cc8b1f2a0ca1233864014260268e3
Eric Schieferstein <erschieferstein@gmail.com>
2025-01-23 12:03:17 -0800

    updates to models and functions for populating db

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -11,16 +11,22 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 class Pitch(Base):
     __tablename__ = "pitches"
     id = Column(Integer, primary_key=True)
-    pitch_type = Column(String)
     pitcher_id = Column(Integer)
     game_id = Column(Integer)
     pitch_number = Column(Integer)
+    pitch_type = Column(String)
+    pitch_result = Column(String)
+    pa_result = Column(String)
+    spin_rate = Column(Integer)
+    pitch_mph = Column(Float)
+    pitch_date = Column(String)
 
 class Pitcher(Base):
     __tablename__ = "pitchers"
     id = Column(Integer, primary_key=True)
     pitcher_id = Column(Integer)
     pitcher_name = Column(String)
+ 
 
 def init_db():
     Base.metadata.create_all(bind=engine)
