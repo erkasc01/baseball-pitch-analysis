6577757a7e2fe0d94ef7f96bbf6f42f3ea2f5864
Eric Schieferstein <erschieferstein@gmail.com>
2025-01-26 17:49:32 -0800

    add pitcher query function

---
+++ main/query_tools.py
@@ -0,0 +1,22 @@
+from main.models import SessionLocal, Pitch, Pitcher
+from collections import defaultdict
+
+# Class takes in pitcher name then returns dic of game pitch type lists
+class PitcherQuery:
+    def __init__(self, pitcherName):
+        self.pitcher_name = pitcherName
+        self.query_results = self.query_for_pitches()
+
+    def query_for_pitches(self) -> defaultdict(list[str]):
+        session = SessionLocal()
+        query = (
+            session.query(Pitch)
+            .join(Pitcher, Pitch.pitcher_id == Pitcher.pitcher_id)
+            .filter(Pitcher.pitcher_name == self.pitcher_name)
+            .order_by(Pitch.game_id.desc(), Pitch.pitch_number.asc())
+        )
+        pitch_dic = defaultdict(list[str])
+        for entry in query:
+            pitch_dic[str(entry.game_id)].append(entry.pitch_type)
+        session.close()
+        return pitch_dic
