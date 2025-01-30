a9359264ab121a9858c1faa1d580374920f00c2b
Eric Schieferstein <erschieferstein@gmail.com>
2025-01-29 16:26:42 -0800

    update formatting of query_tools.py

--- main/query_tools.py
+++ main/query_tools.py
@@ -1,13 +1,14 @@
 from main.models import SessionLocal, Pitch, Pitcher
 from collections import defaultdict
 
+
 # Class takes in pitcher name then returns dic of game pitch type lists
 class PitcherQuery:
     def __init__(self, pitcherName):
         self.pitcher_name = pitcherName
         self.query_results = self.query_for_pitches()
 
-    def query_for_pitches(self) -> defaultdict(list[str]):
+    def query_for_pitches(self) -> defaultdict:
         session = SessionLocal()
         query = (
             session.query(Pitch)
@@ -18,5 +19,6 @@ class PitcherQuery:
         pitch_dic = defaultdict(list[str])
         for entry in query:
             pitch_dic[str(entry.game_id)].append(entry.pitch_type)
+
         session.close()
         return pitch_dic
