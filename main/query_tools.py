2dcb07fed74904fb42f9108a9c2be2bae0fb6a2b
Eric Schieferstein <erschieferstein@gmail.com>
2025-02-01 15:19:11 -0800

    more tools for normalization

--- main/query_tools.py
+++ main/query_tools.py
@@ -5,7 +5,7 @@ from main.models import SessionLocal, Pitch, Pitcher
 # Class takes in pitcher name then returns dic of game pitch type lists
 class PitcherQuery:
     def __init__(self, pitcherName):
-        self.pitcher_name = pitcherName
+        self.pitcher_name = self.get_pitcher_name(pitcherName)
         self.query_results = self.query_for_pitches()
         self.pitcher_name_normalized = self.get_pitcher_name_normalized()
 
@@ -24,6 +24,16 @@ class PitcherQuery:
         session.close()
         return pitch_dic
 
+    def get_pitcher_name(self, p_name):
+        session = SessionLocal()
+        pitcher_query = (
+            session.query(Pitcher)
+            .filter(Pitcher.pitcher_name.ilike(p_name))
+        )
+        for entry in pitcher_query:
+            pitcher_name = entry.pitcher_name
+        return pitcher_name
+
     def get_pitcher_name_normalized(self):
         session = SessionLocal()
         pitcher_query = (
@@ -32,5 +42,5 @@ class PitcherQuery:
         )
         for entry in pitcher_query:
             normalized_name = entry.pitcher_name_normalized
-
+        session.close()
         return normalized_name
