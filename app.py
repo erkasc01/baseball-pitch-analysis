ce969044fdb30f46138ec197eb927a3c778cec64
Eric Schieferstein <erschieferstein@gmail.com>
2025-02-02 18:04:38 -0800

    pretty much working

--- app.py
+++ app.py
@@ -17,17 +17,25 @@ def index():
 
 @app.route("/get-fsm", methods=["GET"])
 def get_pitcher_diagram():
-    pname = request.args.get("pitcher-name")
-    pquery = PitcherQuery(pname)
-    try:
-        mchain = MarkovChain(pquery.query_results)
-        fpath = mchain.save_state_diagram(pname)
-        fpath = fpath.split("static/")[1]
-        return f'<img src="{url_for("static", filename=f"{fpath}")}" alt="State Diagram for {pname}" style="max-width:100%;">'
 
-    except ValueError:
-        print(f"What's wrong with you! {pname} is not an MLB pitcher!!")
-        return "<div></di>"
+    try:
+        p_name = request.args.get("pitcher-name")
+        p_query = PitcherQuery(p_name)
+        p_names = p_query.pitcher_names
+        norm_name = p_names["pitcher_name_normalized"]
+
+        name = p_names["pitcher_name"]
+        m_chain = MarkovChain(p_query.query_results)
+        f_path = m_chain.save_state_diagram(norm_name, name)
+        f_path = f_path.split("static/")[1]
+        return (
+            f'<img src="{url_for("static", filename=f"{f_path}")}" '
+            '"style="max-width:100%;" id="pitch-fsm-result">'
+        )
+
+    except (KeyError):
+        print(f"What's wrong with you! {p_name} is not an MLB pitcher!!")
+        return "<div></div>"
 
 
 if __name__ == "__main__":
